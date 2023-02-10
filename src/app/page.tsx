0bfb1e8a99ef2a684efb96e197ab5377706dc629
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-02-10 13:13:42 +0000

    started typescript practice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex h-screen justify-center items-center">
+      <button
+        type="button"
+        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >Button</button>
+    </div>
   )
 }
